75c604070612bab08d96217e752a454dd9459bec
Antonio Silva <antoniomarcos.silva@protonmail.com>
2026-04-30 10:35:02 -0400

    fix: Method da uri /logout da classe AuthController

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ return [
 	['GET', '/register', [AuthController::class, 'showRegister']],
 	['POST', '/login', [AuthController::class, 'login']],
 	['POST', '/register', [AuthController::class, 'register']],
-	['POST', '/logout', [AuthController::class, 'logout']],
+	['GET', '/logout', [AuthController::class, 'logout']],
 
 	['GET', '/dashboard', [DashboardController::class, 'index']],
 
